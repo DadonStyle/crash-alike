3c7312e26b7976cfd3dafff1f72c3d8016999901
DadonStyle <noamoni9@gmail.com>
2022-03-28 19:59:50 +0300

    websocket so far use the normal one

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
-import WebSocketDemo from './modules/websocketDemo/WebsocketDemo'
+import React, { useEffect } from 'react';
+import { w3cwebsocket as W3CWebSocket } from 'websocket';
+
+const clinet = new W3CWebSocket('ws://147.182.239.36:8080/bomb')
 
 const App = () => {
+
+  useEffect(() => {
+    clinet.opopen = () =>{
+      console.log('web connected');
+    }
+  })
+
+
   return (
     <>
       <div>hellp</div>
-      <WebSocketDemo />
     </>
   )
 }
