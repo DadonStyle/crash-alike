3f46d5c0959fc66329b8646ce9961191bbda45d3
DadonStyle <noamoni9@gmail.com>
2022-04-01 22:27:04 +0300

    adds form

--- src/modules/Login/Login.jsx
+++ src/modules/Login/Login.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+// import { Button, Typography } from "@material-ui/core";
+// import axios from "axios";
+import { Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+// import { useNavigate } from "react-router-dom";
+// import react toastify
+// import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+// import { useState } from "react";
 
 const Login = () => { 
+  // const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  // const [passwordShown, setPasswordShown] = useState(false);
+
+  // const togglePasswordVisibility = () => {
+  //   setPasswordShown(passwordShown ? false : true);
+  // };
+
+  // const send = async () => {
+  //   try {
+  //     const response = await axios.post( 
+  //       "base URL" + "/login" + "/" + "user and password" // check the order
+  //     );
+  //     // notify.success("you have been successfully logged in !!!"); change to react toastify
+  //     navigate.push("/home");
+  //   } catch (err) {
+  //     // notify.error(err.response.data); change to react toastify
+  //   }
+  // }
+
+  const demo = () => {console.log("works")}
+
   return (
-    <div>Login</div>
+    <div className="Login Box">
+    {/* <Typography variant="h3" className="HeadLine">
+      LOGIN
+    </Typography> */}
+    <form onSubmit={handleSubmit(demo)}>
+      <Form.Label>Email address</Form.Label>
+      <Form.Control
+        required
+        type="email"
+        name="email"
+        placeholder="Enter email"
+        {...register('email', { required: true })}
+      />
+      <Form.Label>Password</Form.Label>
+      <Form.Control
+        required
+        type="password"
+        // type={passwordShown ? "text" : "password"}
+        name="password"
+        placeholder="Enter password"
+        {...register('password', { required: true })}
+      />
+      <button
+        variant="outlined"
+        type="submit"
+        className="login_button"
+        size="large"
+      >
+        login
+      </button>
+    </form>
+  </div>
   )
 }
 
--- src/modules/Register/Register.jsx
+++ src/modules/Register/Register.jsx
@@ -1,8 +1,69 @@
 import React from 'react'
+// import { Button, Typography } from "@material-ui/core";
+// import axios from "axios";
+import { Form } from "react-bootstrap";
+import { useForm } from "react-hook-form";
+// import { useNavigate } from "react-router-dom";
+// import react toastify
+// import VisibilityOffIcon from "@material-ui/icons/VisibilityOff";
+// import { useState } from "react";
 
-const Register = () => {
+const Register = () => { 
+  // const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+  // const [passwordShown, setPasswordShown] = useState(false);
+
+  // const togglePasswordVisibility = () => {
+  //   setPasswordShown(passwordShown ? false : true);
+  // };
+
+  // const send = async () => {
+  //   try {
+  //     const response = await axios.post( 
+  //       "base URL" + " Register" + "/" + "user and password" // check the order
+  //     );
+  //     // notify.success("you have been successfully logged in !!!"); change to react toastify
+  //     navigate.push("/home");
+  //   } catch (err) {
+  //     // notify.error(err.response.data); change to react toastify
+  //   }
+  // }
+
+  const demo = () => {console.log("works")}
+
   return (
-    <div>Register</div>
+    <div className="Register Box">
+    {/* <Typography variant="h3" className="HeadLine">
+     Register
+    </Typography> */}
+    <form onSubmit={handleSubmit(demo)}>
+      <Form.Label>Email address</Form.Label>
+      <Form.Control
+        required
+        type="email"
+        name="email"
+        placeholder="Enter email"
+        {...register('email', { required: true })}
+      />
+      <Form.Label>Password</Form.Label>
+      <Form.Control
+        required
+        type="password"
+        // type={passwordShown ? "text" : "password"}
+        name="password"
+        placeholder="Enter password"
+        {...register('password', { required: true })}
+      />
+      <button
+        variant="outlined"
+        type="submit"
+        className= "Register_button"
+        size="large"
+      >
+       Register
+      </button>
+    </form>
+  </div>
   )
 }
 
